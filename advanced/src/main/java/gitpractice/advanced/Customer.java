4f583cf2b9c164a695a4e0b05958e866c08e1ace
kavya654 <kavya.narayan654@gmail.com>
2020-06-30 15:29:19 +0530

    second commit

---
+++ advanced/src/main/java/gitpractice/advanced/Customer.java
@@ -0,0 +1,27 @@
+package gitpractice.advanced;
+
+public class Customer {
+
+    private int cId;
+    private String cname;
+    private String ccourse;
+
+    public int getcId() {
+        return cId;
+    }
+    public void setcId(int cId) {
+        this.cId = cId;
+    }
+    public String getCname() {
+        return cname;
+    }
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+    public String getCcourse() {
+        return ccourse;
+    }
+    public void setCcourse(String ccourse) {
+        this.ccourse = ccourse;
+    }
+}
--- advanced/src/main/java/gitpractice/advanced/CustomerController.java
+++ advanced/src/main/java/gitpractice/advanced/CustomerController.java
@@ -2,13 +2,20 @@ package gitpractice.advanced;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CustomerController {
 
     @RequestMapping("/greet")
-    public String greet(){
-        return "greet";
+    public ModelAndView greet(@RequestParam("cname")String cname) {
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("cname", cname);
+        mv.setViewName("greet");
+
+        return mv;
     }
 
 }
